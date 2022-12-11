4f3a67e8936dd52b95e2aeed6c2eb7fcd4ba7e5c
Antoine Pichard <antoinexvipichard@gmail.com>
2022-12-11 16:33:34 +0100

    _id au lieu de id mais marche tjr pas

--- src/Api/addToPokedex.js
+++ src/Api/addToPokedex.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const AddToPokedex =  async (pokemon) => {
-    console.log('Got body_insert:', pokemon.name);
+    console.log('Got body_insert:', pokemon.name, pokemon._id);
     const url = 'http://localhost:4444/pokedex/insert';
     axios.post(url, null, { params: {
         name: pokemon.name,
-        id: pokemon.id,
+        _id: pokemon._id,
     }})
     .then(response => console.log(response.status))
     .catch(err => console.warn(err));
